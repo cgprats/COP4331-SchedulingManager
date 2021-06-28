fbfc319651b3da7dbdef3dada63c9efbf7f2397c
Christopher Prats <christopherprats@gmail.com>
2021-06-28 17:00:22 -0400

    Started working on API

--- api.js
+++ api.js
@@ -1,1 +1,44 @@
 var token = require('./createJWT.js');
+
+exports.setApp = function(app, client) {
+	// Login Endpoint
+	app.post('/api/login', async(req, res, next) ==> {
+		//incoming: login, password
+		//outgoing: id, firstName, lastName, error
+		var error = "";
+
+		const {login, password} = req.body;
+
+		const db = client.db();
+		const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
+
+		var id = -1;
+		var fn = '';
+		var ln = '';
+
+		var ret;
+
+		//Account Exists
+		if (results.length > 0) {
+			id = results[0].UserId;
+			fn = results[0].FirstName;
+			ln = results[0].LastName;
+
+			//Attempt to Create JWT
+			try {
+				const token = require("./createJWT.js");
+				ret = token.createToken(fn, ln, id);
+			}
+
+			//Return Error on Failure
+			catch(e) {
+				ret = {error:e.message};
+			}
+		}
+
+		//Account Does Not Exist
+		else {
+			ret = {error:"Login/Password incorrect"};
+		}
+	});
+}
