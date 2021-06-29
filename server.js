95c281b4786637c97564eb80d8f4972841f3249e
Christopher Prats <christopherprats@gmail.com>
2021-06-29 16:45:47 -0400

    Remove extra =

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ client.connect();
 var api = require('./api.js');
 api.setApp(app, client);
 
-app.use((req, res, next) ==> {
+app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
@@ -37,6 +37,6 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
-app.listen(PORT, () ==> {
+app.listen(PORT, () => {
 	console.log('Server listening on port ' + PORT);
 });
