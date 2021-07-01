c4e82eed3729cace9532283829bda81760b12ad7
snowbowls <61566230+snowbowls@users.noreply.github.com>
2021-07-01 13:14:06 -0400

    back tracking

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+  
+import logo from './logo.svg';
 import './App.css';
-
-import LoginPage from './pages/LoginPage';
-import CardPage from './pages/CardPage';
 
 function App() {
   return (
-    <Router >
-      <Switch>
-        <Route path="/" exact>
-          <LoginPage />
-        </Route>
-        <Route path="/cards" exact>
-          <CardPage />
-        </Route>
-        <Redirect to="/" />
-      </Switch>  
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
