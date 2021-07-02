f1698f39f7e559f17e4d2cee91f089d2f1c0c135
Christopher Prats <christopherprats@gmail.com>
2021-07-02 14:02:25 -0400

    Added export to App class

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // Removed logo since we don't need it anymore
 import './App.css';
-export class App extends Component {
+class App extends Component {
   render() {
     return (
       <div classname="container">
@@ -11,3 +11,5 @@ export class App extends Component {
     );
   }
 }
+
+export default App;
