e2cd1c544709b0d1a158f9a0a8502580f2dcafb6
Christopher Prats <christopherprats@gmail.com>
2021-07-02 13:47:42 -0400

    Prototyped APIs

--- api.js
+++ api.js
@@ -1,16 +1,12 @@
 //var token = require('./createJWT.js');
 
 exports.setApp = function(app, client) {
+	app.post('/api/login', async (req, res, next) => {
+		// incoming: login, password
+		// outgoing: id, firstName, lastName, error
+		var errorMessage = '';
 
-	// Sorry Chirs, this thing was causing me issues. Revert back whenever
-	/*
-	// Login Endpoint
-	app.post('/api/login', async(req, res, next) => {
-		//incoming: login, password
-		//outgoing: id, firstName, lastName, error
-		var error = "";
-
-		const {login, password} = req.body;
+		const { login, password } = req.body;
 
 		const db = client.db();
 		const results = await db.collection('workers').find({Login:login,Password:password}).toArray();
@@ -19,93 +15,94 @@ exports.setApp = function(app, client) {
 		var fn = '';
 		var ln = '';
 
-		var ret;
-
 		//Account Exists
-		if (results.length > 0) {
+		if (results.length > 0)
+		{
 			id = results[0].UserId;
 			fn = results[0].FirstName;
 			ln = results[0].LastName;
 
-			//Attempt to Create JWT
-			try {
-				const token = require("./createJWT.js");
-				ret = token.createToken(fn, ln, id);
-			}
-
-			//Return Error on Failure
-			catch(e) {
-				ret = {error:e.message};
-			}
+			//TODO: Create JWT
 		}
 
-		//Account Does Not Exist
 		else {
-			ret = {error:"Login/Password incorrect"};
+			errorMessage = "Login/Password incorrect";
 		}
+
+		var ret = { id:id, firstName:fn, lastName:ln, error:errorMessage};
+		res.status(200).json(ret);
 	});
 
-	// Register Endpoint
 	app.post('/api/register', async(req, res, next) => {
-		//incoming: login, password, first name, last name
-		//outgoing: id, error
-		
-		var login = req.body.login; // Should we use e-mail instead?
-		var password = req.body.password;
+		// incoming: login, password
+		// outgoing: id, error
 		var email = req.body.email;
+		var password = req.body.password;
 		var FirstName = req.body.FirstName;
-    		var LastName = req.body.LastName;
-		
+		var LastName = req.body.LastName;
+
+		//TODO: Handle password confirm
+
 		var data = {
-			"login" : login,
-        		"password" : password,
-			"email" : email,
-			"FirstName" : FirstName,
-			"LastName" : LastName
+			"login": login,
+			"password": password,
+			"FirstName": FirstName,
+			"LastName": LastName
 		}
-		
+
+		//TODO: Handle insert error
 		const db = client.db();
-		const results = await db.collection('workers').insertOne(data,function(err, collection){ 
-			// Check for conflict
-		}
-    		
-    		return res.redirect( ... );
-	}); 
-	
-	*/
-	app.post('/api/login', async (req, res, next) => 
-	{
-      // incoming: login, password
-      // outgoing: id, firstName, lastName, error
-    
-     var error = '';
-    
-      const { login, password } = req.body;
-    
-      const db = client.db();
-      const results = await db.collection('workers').find({Login:login,Password:password}).toArray();
-    
-      var id = -1;
-      var fn = '';
-      var ln = '';
-    
-      if( results.length > 0 )
-      {
-        id = results[0].UserId;
-        fn = results[0].FirstName;
-        ln = results[0].LastName;
-      }
-    
-      var ret = { id:id, firstName:fn, lastName:ln, error:''};
-      res.status(200).json(ret);
-    });
+		const results = await db.collection('workers').insertOne(data, function(err, collection) {
+		});
+
+		// Login after successful registration
+		return res.redirect('/api/login');
+	});
 
 	app.post('/api/addorder', async(req, res, next) => {
+		//TODO: Determine necessary data types for orders
+		// incoming:
+		// outgoing:
+
+		var data = {
+		}
+
+		//TODO: Handle insert error
+		const db = client.db();
+		const results = await db.collection('orders').insertOne(data, function(err, collection) {
+		});
+
+		var ret = {};
+		res.status(200).json(ret);
+
 	});
-	
+
 	app.post('/api/editorder', async(req, res, next) => {
+		//TODO: See addorder
+		// incoming:
+		// outgoing:
+		
+		var data = {
+		}
+
+		//TODO: Handle update error
+		const db = client.db();
+		const results = await db.collection('orders').updateOne( { filter:filter_var }, data, {upsert: false});
+
+		var ret = {};
+		res.status(200).json(ret);
 	});
-	
+
 	app.post('/api/deleteorder', async(req, res, next) => {
+		//TODO: See addorder
+		// incoming:
+		// outgoing:
+
+		//TODO: Handle delete error
+		const db = client.db();
+		const results = await db.collection('orders').deleteOne( { data:data_var });
+		
+		var ret = {};
+		res.status(200).json(ret);
 	});
 }
