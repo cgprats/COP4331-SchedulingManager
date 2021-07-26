5bb1c68de9224cbe72b1f5f439224dfefb3a9f4a
Joel Joy <joeljoy1912@knights.ucf.edu>
2021-07-26 15:58:37 -0400

    Updating employee search

--- mobile/lib/components/employee_card.dart
+++ mobile/lib/components/employee_card.dart
@@ -22,7 +22,109 @@ class _EmployeeCardState extends State<EmployeeCard> {
   Widget build(BuildContext context) {
     return FractionallySizedBox(
       widthFactor: widget.width,
-
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        child: Table(
+          columnWidths: const <int,
+              TableColumnWidth>{
+            0: FractionColumnWidth(0.40),
+          },
+          children: <TableRow>[
+            TableRow(
+              children: <TableCell>[
+                TableCell(
+                  child: Container(
+                    height: 30,
+                    child: Text(
+                      'Name:',
+                      style: TextStyle(
+                        color: CustomColors.white,
+                        fontWeight:
+                        FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                TableCell(
+                  child: Container(
+                    height: 30,
+                    child: Text(
+                      GlobalData.firstName! + ' ' + GlobalData.lastName!,
+                      style: TextStyle(
+                        color: CustomColors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            TableRow(
+              children: <TableCell>[
+                TableCell(
+                  child: Container(
+                    height: 30,
+                    padding:
+                    EdgeInsets.only(top: 2.5),
+                    child: Text(
+                      'Email:',
+                      style: TextStyle(
+                        color: CustomColors.white,
+                        fontWeight:
+                        FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                TableCell(
+                  child: Container(
+                    height: 30,
+                    padding:
+                    EdgeInsets.only(top: 2.5),
+                    child: Text(
+                      GlobalData.email!,
+                      style: TextStyle(
+                        color: CustomColors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            TableRow(
+              children: <TableCell>[
+                TableCell(
+                  child: Container(
+                    height: 30,
+                    padding:
+                    EdgeInsets.only(top: 2.5),
+                    child: Text(
+                      'Phone:',
+                      style: TextStyle(
+                        color: CustomColors.white,
+                        fontWeight:
+                        FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                TableCell(
+                  child: Container(
+                    height: 30,
+                    padding:
+                    EdgeInsets.only(top: 2.5),
+                    child: Text(
+                      GlobalData.phone!,
+                      style: TextStyle(
+                        color: CustomColors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
     );
   }
 }
--- mobile/lib/screens/employee_search_screen.dart
+++ mobile/lib/screens/employee_search_screen.dart
@@ -9,15 +9,20 @@ import 'package:mobile/utils/get_api.dart';
 import 'package:mobile/utils/custom_colors.dart';
 import 'package:mobile/components/rounded_button.dart';
 import 'package:mobile/components/custom_scaffold.dart';
+import 'package:mobile/components/employee_card.dart';
 import 'package:mobile/components/textfield_widget.dart';
 
-class EmployeeSearchScreen extends StatefulWidget{
+class EmployeeSearchScreen extends StatefulWidget {
   @override
   _EmployeeSearchScreenState createState() => _EmployeeSearchScreenState();
 }
 
 class _EmployeeSearchScreenState extends State<EmployeeSearchScreen> {
   String? _input;
+  String _errorMessage = '';
+  List<EmployeeCard> employees = [];
+
+
   @override
   Widget build(BuildContext context) {
     Size _size = MediaQuery.of(context).size;
@@ -25,36 +30,74 @@ class _EmployeeSearchScreenState extends State<EmployeeSearchScreen> {
       title: 'Employee Search',
       appBarColor: CustomColors.orange,
       body: Container(
-        child: Column(
-          children: <Widget>[
-            RoundedInputField(
-              hintText: 'Search...',
-              margin: EdgeInsets.zero,
-              onChanged: (value) {
-                this._input = value;
-              },
-              textInputAction: TextInputAction.done,
-              onFieldSubmitted: (value) {
-                _search();
-              },
-              suffixIcon: IconButton(
-                icon: Icon(
-                  Icons.search,
-                  color: CustomColors.orange,
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        decoration: BoxDecoration(
+          color: CustomColors.purple,
+        ),
+        child: Center(
+          child: ListView(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RoundedInputField(
+                hintText: 'Search...',
+                margin: EdgeInsets.zero,
+                width: _size.width * 0.8,
+                onChanged: (value) {
+                  this._input = value;
+                },
+                textInputAction: TextInputAction.done,
+                onFieldSubmitted: (value) {
+                  _search();
+                },
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.search,
+                    color: CustomColors.orange,
+                  ),
+                  onPressed: _search,
                 ),
-                onPressed: _search,
               ),
-            )
-          ],
-        )
-      )
+              //employees,
+            ],
+          ),
+        ),
+      ),
     );
   }
 
-  void _search() {
+  void _search() async {
     print('search!');
     Map _payload = Map();
     _payload['input'] = this._input;
     _payload['compCode'] = GlobalData.companyCode;
+    String dir = '/searchWorkers';
+    String ret = await API.getJson(dir, _payload);
+    var jsonObj = json.decode(ret);
+    //print(jsonObj);
+    if (ret.isEmpty) {
+      print('oh no :(');
+    } else {
+      setState(
+        () {
+          print('searchJobs successful!');
+          _errorMessage =
+              jsonObj['error'] == 'Job added!' ? '' : jsonObj['error'];
+        },
+      );
+    }
+  }
+  void addEmployee() {
+    employees.add(sample1());
+  }
+  EmployeeCard sample1() {
+    return EmployeeCard(
+      width: 0.8,
+      clientInfo: {
+        'firstName': 'Bobby',
+        'lastName': 'Dylan',
+        'email': 'BobDill@gmail.com',
+        'phone': '305-519-8560',
+      },
+    );
   }
 }
